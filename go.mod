0a6dc7fe0ceb3f5115dfc296244463ab59ed41d4
neustroevr <neustroev.r@indriver.com>
2024-03-12 17:55:53 +0700

    remove graphql

--- go.mod
+++ go.mod
@@ -9,7 +9,7 @@ require (
 	go.uber.org/fx v1.20.1
 	go.uber.org/zap v1.23.0
 	google.golang.org/grpc v1.61.0
-	google.golang.org/protobuf v1.32.0
+	google.golang.org/protobuf v1.33.0
 )
 
 require (
@@ -19,18 +19,17 @@ require (
 	github.com/go-openapi/spec v0.20.14 // indirect
 	github.com/go-openapi/swag v0.22.9 // indirect
 	github.com/golang/protobuf v1.5.3 // indirect
-	github.com/graphql-go/graphql v0.8.1 // indirect
-	github.com/graphql-go/handler v0.2.3 // indirect
 	github.com/josharian/intern v1.0.0 // indirect
 	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/stretchr/testify v1.9.0 // indirect
 	github.com/swaggo/files v1.0.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/dig v1.17.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
-	golang.org/x/net v0.20.0 // indirect
-	golang.org/x/sys v0.16.0 // indirect
+	golang.org/x/net v0.22.0 // indirect
+	golang.org/x/sys v0.18.0 // indirect
 	golang.org/x/text v0.14.0 // indirect
-	golang.org/x/tools v0.17.0 // indirect
+	golang.org/x/tools v0.19.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20240123012728-ef4313101c80 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/server/http/graph.go
+++ internal/server/http/graph.go
@@ -1,64 +1,16 @@
 package http
 
-import (
-	"github.com/graphql-go/graphql"
-	"github.com/graphql-go/handler"
-)
-
 type Graphql struct {
-	schema graphql.Schema
 }
 
 func NewGraphql() (*Graphql, error) {
-	cfg := graphql.SchemaConfig{
-		Query: &graphql.Object{
-			PrivateName:        "",
-			PrivateDescription: "",
-			IsTypeOf:           nil,
-		},
-		Mutation: &graphql.Object{
-			PrivateName:        "",
-			PrivateDescription: "",
-			IsTypeOf:           nil,
-		},
-		Subscription: &graphql.Object{
-			PrivateName:        "",
-			PrivateDescription: "",
-			IsTypeOf:           nil,
-		},
-		Types: []graphql.Type{},
-		Directives: []*graphql.Directive{
-			{
-				Name:        "",
-				Description: "",
-				Locations:   nil,
-				Args:        nil,
-			},
-		},
-		Extensions: []graphql.Extension{},
-	}
-
-	schema, err := graphql.NewSchema(cfg)
-	if err != nil {
-		return nil, err
-	}
-
-	return &Graphql{schema: schema}, nil
+	return &Graphql{}, nil
 }
 
-func (g *Graphql) Handler() *handler.Handler {
-	return handler.New(&handler.Config{
-		Schema:   &g.schema,
-		Pretty:   true,
-		GraphiQL: true,
-	})
+func (g *Graphql) Handler() {
+
 }
 
-func (g *Graphql) Playground() *handler.Handler {
-	return handler.New(&handler.Config{
-		Schema:     &g.schema,
-		Pretty:     true,
-		GraphiQL:   false,
-		Playground: true,
-	})
+func (g *Graphql) Playground() {
+
 }
